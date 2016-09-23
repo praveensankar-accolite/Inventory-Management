6edd37c8630d2b2635bb7b12fe4d76b83ef1f296
sridhar3525 <sridharbabu1138@gmail.com>
2016-09-23 20:51:49 +0530

    ControllerDone

--- InventoryManagementWEB/InventoryManagementWEB/Controllers/ManagerController.cs
+++ InventoryManagementWEB/InventoryManagementWEB/Controllers/ManagerController.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
 using Newtonsoft.Json;
+using InventoryAPP.Controllers;
 
 namespace InventoryManagementWEB.Controllers
 {
@@ -26,14 +30,70 @@ namespace InventoryManagementWEB.Controllers
             return View();
         }
 
-        public ActionResult GetHistoryDetails(string jsondata)
+        public void GetHistoryDetails(string jsondata)
         {
             //dynamic DynamicObj = JsonConvert.DeserializeObject(obj);
             dynamic dynobj = JsonConvert.DeserializeObject(jsondata);
             //Object DynamicObj = JsonConvert.DeserializeObject(obj);
-            return View();
+            string google = "http://www.google.co.in";
+            string url = "http://localhost:49962/api/Tickets/GetTicket/1";
+            
+            //return Redirect("http://localhost:49962/api/Tickets/GetTicket/1");
+            RedirectResult RR = Redirect(url);
+            //Response.Redirect(url, true);
+            //AR = RedirectToAction(url);
+            //Uri uri;
+
+            //HttpClient client = new HttpClient();
+            //client.BaseAddress = new Uri(url);
+            //client.Dispose();
+
+
+            //function();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:49962/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Main Code
+                HttpResponseMessage response = client.GetAsync("api/Tickets/GetTicket/1").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    dynamic product = response.Content.ReadAsAsync<dynamic>().Result;
+                    //dynamic Result = product.Result;
+                    string result = product.name;
+                    string str = product.foo;
+                }
+            }
+
+            
+            //InventoryAPP.Controllers.TicketsController Obj = new TicketsController();
+            //Obj.GetTicket(1);
+            RedirectToAction("Index", "Home");
+            return;
+            //return View();
         }
-        // GET: Manager/Details/5
+
+        public async void function()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:49962/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Main Code
+                HttpResponseMessage response = await client.GetAsync("api/Tickets/GetTicket/1");
+                if (response.IsSuccessStatusCode)
+                {
+                    dynamic product = await response.Content.ReadAsAsync<dynamic>();
+                    string str = product.msg;
+                }
+            }
+        }
+        // : Manager/Details/5
         public ActionResult Details(int id)
         {
             return View();
